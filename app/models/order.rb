c69ac9206d272e119c6e9ccdd219f854e2cd5352
sofiachakir <sofiachakir@gmail.com>
2020-08-03 18:58:20 +0200

    add revenue calculations

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,4 +10,18 @@ class Order < ApplicationRecord
 		order = Order.find_by(order_id: params['order_id'])
 		order ? order : Order.create(order_params)
 	end
+
+	def revenue
+		res = self.order_product_quantities.joins(:product).pluck(:quantity, :unit_price)
+		res.map{|q, up| q * up}.sum
+	end
+
+	def self.total_revenue
+		Order.all.map{ |o| o.revenue }.sum
+	end
+
+	def self.average_revenue_per_order
+		order_count = Order.count
+		order_count ? (Order.total_revenue / order_count) : 0
+	end
 end
